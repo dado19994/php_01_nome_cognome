82089db36771892e124b64b6e3e398dc3c947d6d
davidedignazio <davidedignazio@MacBook-Pro-di-Davide.local>
2025-02-03 22:30:04 +0100

    php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<html></html>
+<?php
+$boolean = true; //=> Stampa il numero 1
+$boolean = false; //=> false == 0 == null --- non stampa nulla
+
+//* echo => stampa il contenuto solo di valori primitivi e convertibili in stringa
+echo $int;
+var_dump($int); //* ci permette di stampare il valore della variabile più il suo tipo
+
+?>
